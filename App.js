5c4cd22a6380edc972218ee6dd7627dc08791024
Samacriano <samacriano@gmail.com>
2022-05-19 07:15:19 +0100

    firebase and redux implementation

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants';
+import firebase from 'firebase/app';
+
+if(firebase.apps.lenght == 0) {
+  firebase.initializeApp(Constants.manifest.web.config.firebase);
+}
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Samacriano Mendes</Text>
       <StatusBar style="auto" />
     </View>
   );
